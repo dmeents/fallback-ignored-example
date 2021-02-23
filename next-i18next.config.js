c6df8ecbcce8c6d66a2fa55dee0350f3cb49d24d
David <david@allido.io>
2021-02-23 08:38:27 -0800

    update configs to match recommendations in beta6 documentation

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  defaultLocale: 'en-US',
+  locales: ['en-US', 'es-MX',],
   fallbackLng: 'en-US',
   cleanCode: true,
 };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
+const i18n = require('./next-i18next.config');
+
 module.exports = {
-  i18n: {
-    defaultLocale: 'en-US',
-    locales: ['en-US', 'es-MX',],
-  },
+  i18n,
 }
